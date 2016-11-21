5d1f33216e4410d9de657c37d6d4d02cdf0ff71e
Josef Zamrzla <josef.zamrzla@gmail.com>
2016-11-21 12:16:39 +0100

    add mocha test

--- test.js
+++ test.js
@@ -1,3 +1,26 @@
 var getHeapSpaceStatistics = require('./');
 
-console.log(getHeapSpaceStatistics());
+//console.log(getHeapSpaceStatistics());
+var assert = require('assert');
+describe('v8-heap-space-statistics', function() {
+
+	it('should return not empty array', function() {
+		var stats = getHeapSpaceStatistics();
+		assert.ok(Array.isArray(stats), 'Returned value is not an array');
+		assert.ok(stats.length > 0, 'Returned array is empty');
+	});
+
+	it('should return array of objects with required list of properties', function() {
+		getHeapSpaceStatistics().forEach(function (space) {
+			assert.ok(!!space, 'Stats object is empty');
+			assert.ok(
+				'space_name' in space &&
+				'space_size' in space &&
+				'space_used_size' in space &&
+				'space_available_size' in space &&
+				'physical_space_size' in space,
+				'Space object does not contain all required properties'
+			);
+		});
+	});
+});
